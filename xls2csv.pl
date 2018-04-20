f189a862c8af4c53e5ce41038648122934bd6b77
kijato <kijato@gmail.com>
2018-04-20 09:43:51 +0200

    Add files via upload

---
+++ xls2csv.pl
@@ -0,0 +1,38 @@
+#!C:\perl\perl.exe
+#!/usr/bin/perl -w
+#!C:\programs\perl\perl.exe -w
+
+use strict;
+use warnings;
+use Spreadsheet::ParseExcel;
+use encoding "LATIN2";
+
+my $fajl=$ARGV[0] || die "Hiányzó paraméter!\n";
+my $formated = $ARGV[1] || 1;
+my $separator = $ARGV[2] || ';';
+
+my $parser   = Spreadsheet::ParseExcel->new();
+my $workbook = $parser->parse($fajl);
+if ( !defined $workbook ) { die $parser->error(), ".\n"; }
+
+for my $worksheet ( $workbook->worksheets() ) {
+  my ( $row_min, $row_max ) = $worksheet->row_range();
+  my ( $col_min, $col_max ) = $worksheet->col_range();
+  #print "Sheet name: ".$worksheet->{Name}."\n";
+  for my $row ( $row_min .. $row_max ) {
+    print $worksheet->{Name}." [",$row+1,"];";
+    for my $col ( $col_min .. $col_max ) {
+      my $cell = $worksheet->get_cell( $row, $col );
+      #next unless $cell;
+      unless ($cell) { print $separator; next; }
+      #print "Row, Col    = ($row, $col)\t";
+      if ($formated) {
+        print $cell->value();
+      } else {
+        print $cell->unformatted();
+      }
+      print $separator;
+    }
+    print "\n";
+  }
+}
